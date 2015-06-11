14584bca8ea1150c0a52a9c9d2a3f2a59fc01419
Stuart Varrall <git@fpstudios.com>
2015-06-11 21:41:39 +0100

    Initial Commit

---
+++ complication WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  complication WatchKit Extension
+//
+//  Created by Stuart Varrall on 11/06/2015.
+//  Copyright © 2015 Stuart Varrall. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ complication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  complication
+//
+//  Created by Stuart Varrall on 11/06/2015.
+//  Copyright © 2015 Stuart Varrall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
